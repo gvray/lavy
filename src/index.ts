aee01c678874372709816b5b648ce92b1134cae5
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-05-14 15:25:58 +0800

    Remove src/*

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const eslint = require('./eslint')
-const stylelint = require('./stylelint')
-const prettier = require('./prettier')
-
-module.exports = {
-  eslint,
-  stylelint,
-  prettier
-}
--- src/stylelint.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['stylelint-config-standard'],
-  plugins: ['stylelint-prettier'],
-  rules: {
-    'prettier/prettier': true
-    // add your own custom rules here
-  }
-}
